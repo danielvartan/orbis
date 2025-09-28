c330271daaac50c246fbd624f5057f465e1c51f0
Daniel Vartanian <danielvartan@proton.me>
2025-09-28 01:12:02 -0300

    Add `worldclim_extract_*()`

--- NEWS.md
+++ NEWS.md
@@ -3,7 +3,7 @@
 - Moved several dependencies to *Suggests* in the DESCRIPTION file.
 - Removed `get_wc_osf_id()` function as it is no longer needed.
 - Added `unique_outliers()` and `remove_unique_outliers()` to handle unique outliers in data.
-- Added `worldclim_random()` to deal with [WorldClim](https://www.worldclim.org/) data.
+- Added `worldclim_random()` and `worldclim_extract_*()` to deal with [WorldClim](https://www.worldclim.org/) data.
 - Changed package license from [MIT](https://opensource.org/license/mit) to [GPLv3](https://www.gnu.org/licenses/gpl-3.0).
 - Updated the Code of Conduct to [Contributor Covenant 3.0](https://www.contributor-covenant.org/version/3/0/code_of_conduct/).
 - Improved error handling and messaging throughout the package.
---
+++ R/worldclim_extract.R
@@ -0,0 +1,127 @@
+#' Extract components from WorldClim filenames
+#'
+#' @description
+#'
+#' `worldclim_extract_*()` extract components present in WorldClim filenames.
+#'
+#'
+#' @param file A [`character`][base::character()] vector with WorldClim
+#'   filenames.
+#'
+#' @return A [`character`][base::character()] vector with the extracted
+#'   component.
+#'
+#' @family WorldClim functions
+#' @export
+#'
+#' @examples
+#' files <- c(
+#'   "wc2.1_10m_tavg_1970-2000-06.asc",
+#'   "wc2.1_cruts4.09_30s_tmin_1962-10.asc",
+#'   "wc2.1_5m_tmax_GISS-E2-1-G_ssp370_2081-2100-05.asc"
+#' )
+#'
+#' files |> worldclim_extract_variable()
+#' #> [1] "tavg" "tmin" "tmax"
+#'
+#' files |> worldclim_extract_resolution()
+#' #> [1] "10m" "30s" "5m"
+#'
+#' files |> worldclim_extract_month()
+#' #> [1] "06" "10" "05"
+#'
+#' files |> worldclim_extract_year()
+#' #> [1] "1970-2000" "1962"      "2081-2100"
+#'
+#' files |> worldclim_extract_year_month()
+#' #> [1] "1970-2000-06" "1962-10"      "2081-2100-05"
+#'
+#' files |> worldclim_extract_year_group()
+#' #> [1] NA         NA         "2081-2100"
+#'
+#' files |> worldclim_extract_gcm()
+#' #> [1] NA            NA            "GISS-E2-1-G"
+#'
+#' files |> worldclim_extract_ssp()
+#' #> [1] NA       NA       "ssp370"
+worldclim_extract_variable <- function(file) {
+  checkmate::assert_character(file)
+
+  file |> stringr::str_extract("(?<=(m|s)_)[a-z]*(?=_)")
+}
+
+#' @rdname worldclim_extract_variable
+#' @export
+worldclim_extract_resolution <- function(file) {
+  checkmate::assert_character(file)
+
+  file |> stringr::str_extract("(?<=_)\\d.*(m|s)(?=_)")
+}
+
+#' @rdname worldclim_extract_variable
+#' @export
+worldclim_extract_month <- function(file) {
+  checkmate::assert_character(file)
+
+  ifelse(
+    worldclim_extract_variable(file) %in% c("bioc", "elev"),
+    NA_character_,
+    file |> stringr::str_extract("(?<=-)\\d{2}(?=.[A-Za-z])")
+  )
+}
+
+#' @rdname worldclim_extract_variable
+#' @export
+worldclim_extract_year <- function(file) {
+  checkmate::assert_character(file)
+
+  ifelse(
+    is.na(worldclim_extract_year_group(file)),
+    file |> stringr::str_extract("(?<=_)\\d{4}(?=-\\d{2}.[A-Za-z])"),
+    worldclim_extract_year_group(file)
+  )
+}
+
+#' @rdname worldclim_extract_variable
+#' @export
+worldclim_extract_year_month <- function(file) {
+  checkmate::assert_character(file)
+
+  year <- ifelse(
+    is.na(worldclim_extract_year_group(file)),
+    file |> stringr::str_extract("(?<=_)\\d{4}(?=-\\d{2}.[A-Za-z])"),
+    worldclim_extract_year_group(file)
+  )
+
+  month <- worldclim_extract_month(file)
+
+  ifelse(
+    worldclim_extract_variable(file) %in% c("bioc", "elev"),
+    NA_character_,
+    paste(year, month, sep = "-")
+  )
+}
+
+#' @rdname worldclim_extract_variable
+#' @export
+worldclim_extract_year_group <- function(file) {
+  checkmate::assert_character(file)
+
+  file |> stringr::str_extract("\\d{4}-\\d{4}")
+}
+
+#' @rdname worldclim_extract_variable
+#' @export
+worldclim_extract_gcm <- function(file) {
+  checkmate::assert_character(file)
+
+  file |> stringr::str_extract("(?<=_)[A-Z].+(?=_ssp\\d{3})")
+}
+
+#' @rdname worldclim_extract_variable
+#' @export
+worldclim_extract_ssp <- function(file) {
+  checkmate::assert_character(file)
+
+  file |> stringr::str_extract("ssp\\d{3}")
+}
